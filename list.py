6b7a8e246434b97868663e6ddd9ba3cdb2bb12eb
AlbinDavis <lbndvs@gmail.com>
2021-03-07 18:27:32 +0530

    2

--- list.py
+++ list.py
@@ -1,4 +1,20 @@
-
-li=[10,30,-50,-44]
-li.sort()
-print(li)
+n = int(input())
+lis = []
+for i in range(n):
+    str=input()
+    li = []
+    li = str.split()
+    if li[0]=="append":
+        lis.append(int(li[1]))
+    elif li[0]=="insert":
+        lis.insert(int(li[1]),int(li[2]))
+    elif li[0]=="remove":
+        lis.remove(int(li[1]))
+    elif li[0]=="print":
+        print(lis)
+    elif li[0]=="sort":
+        lis.sort()
+    elif li[0]=="reverse":
+        lis.reverse()
+    elif li[0]=="pop":
+        lis.pop()
