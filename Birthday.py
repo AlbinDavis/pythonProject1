8b56cda0df630ec69dae5278618e0748a64800f0
AlbinDavis <lbndvs@gmail.com>
2021-03-09 10:51:15 +0530

    new

--- Birthday.py
+++ Birthday.py
@@ -1,19 +1,24 @@
-# d=3
-# s=[1,2,1,3,2]
-# list(map(int,s))
-# m=2
-#
-# c1 = 0
+d=3
+s=[1,2,1,3,2]
+list(map(int,s))
+m=2
+
+c1 = 0
+for i in range(len(s)-m+1):
+    c = 0
+    r = 0
+    j = i
+    for j in range(m):
+        r+=s[i+j]
+        c+=1
+        if r == d:
+            c1 += 1
+print(c1)
+
+# c = 0
 # for i in range(len(s)-m+1):
-#     c = 0
-#     r = 0
-#     j = i
-#     for j in range(m):
-#         r+=s[i+j]
-#         c+=1
-#         if r == d and c==m:
-#             c1 += 1
-# print(c1)
-# if sum(squares[i:i+m]) == d:
-#             count+=1
-#     return count
+#
+#         r = 0
+#         if sum(s[i:i+m])==d:
+#                 c+=1
+# print(c)
