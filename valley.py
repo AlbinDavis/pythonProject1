eb7c2f9aa02ba691be77aee6f0f67041891bce3c
AlbinDavis <lbndvs@gmail.com>
2021-03-10 13:15:16 +0530

    new

--- valley.py
+++ valley.py
@@ -1,1 +1,12 @@
-print(1//2)
+step=int(input())
+path=input()
+s = 0
+v = 0
+for i in path:
+    if s == 0 and i == "D":
+        v += 1
+    if i == "U":
+        s += 1
+    else:
+        s -= 1
+print(v)
