da90e140eb574efebdfb3fb684e6b55f6716f6bc
AlbinDavis <lbndvs@gmail.com>
2021-03-12 20:10:54 +0530

    new

--- rotation.py
+++ rotation.py
@@ -1,3 +1,35 @@
+# Python3 implementation of right rotation
+# of an array K number of times
+
+# Function to rightRotate array
+def RightRotate(a, n, k):
+    # If rotation is greater
+    # than size of array
+    k = k % n;
+
+    for i in range(0, n):
+
+        if (i < k):
+
+            # Printing rightmost
+            # kth elements
+            print(a[n + i - k], end=" ")
+
+        else:
+
+            # Prints array after
+            # 'k' elements
+            print(a[i - k], end=" ")
+
+    print("\n")
+
+
+Array = [1, 2, 3, 4, 5]
+N = len(Array)
+K = 2
+
+RightRotate(Array, N, K)
+
 # i=0
 # j=0
 # k = int(input())
@@ -21,4 +53,11 @@
 #
 # print(l[-x:] + l[:-x]) to right
 # print(l[x:] + l[:x]) to left
+
+# Using collection Module
+
+# arr=deque(arr)
+#from collections import deque
+# arr.rotate(k)
+
 
