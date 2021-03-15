c91c36afed2f77dcafff78fd27a713e52392331d
AlbinDavis <lbndvs@gmail.com>
2021-03-15 23:43:44 +0530

    new

---
+++ utopian tree.py
@@ -0,0 +1,17 @@
+
+def findDigits(n):
+    li=str(n)
+    c=0
+    for i in li:
+        if int(i)!=0 and n%int(i)==0:
+            c+=1
+    return c
+
+
+
+t = int(input())
+
+for t_itr in range(t):
+    n = int(input())
+
+    result = findDigits(n)
