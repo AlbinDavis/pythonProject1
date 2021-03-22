0717ebff8863093de37e5e6154a2eeabd05d6c35
AlbinDavis <lbndvs@gmail.com>
2021-03-22 07:54:32 +0530

    new

--- left and right shift.py
+++ left and right shift.py
@@ -1,7 +1,7 @@
-s=input()
-l=s[1:]+s[0]
-r=s[-1]+s[:-1]
-if l==r:
+s = input()
+l = s[1:] + s[0]
+r = s[-1] + s[:-1]
+if l == r:
     print(1)
 else:
     print(0)
--- sqr2.py
+++ sqr2.py
@@ -3,13 +3,13 @@ if n < 1:
     print("incorrect input")
 else:
     n1 = n * n
-    c=1
+    c=0
     while(n>0):
         if n%10 != n1%10:
-            c=0
+            c=1
         n=int(n/10)
         n1=int(n1/10)
-    if c == 1:
+    if c == 0:
         print("yes")
     else:
         print("no")
