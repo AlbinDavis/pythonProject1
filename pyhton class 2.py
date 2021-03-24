a924ca82145a911631de295c3d6933b7a4b8151e
AlbinDavis <lbndvs@gmail.com>
2021-03-24 20:52:45 +0530

    new

---
+++ pyhton class 2.py
@@ -0,0 +1,12 @@
+class Compl:
+    def __init__(self,r,i):
+        self.r=r
+        self.i=i
+    def s(self,num):
+        real=self.r+num.r
+        img=self.i+num.i
+        print(real,"+i",img,sep="")
+c1=Compl(2,3)
+c2=Compl(4,5)
+c1.s(c2)
+#Compl.s(c1,c2)
