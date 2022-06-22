1990e8cbb720047097cbf3d791532487e0c5353a
Albin Davis <lbndvs@gmail.com>
2022-06-22 12:23:44 +0530

    new codes

--- venv/leetcode1.py
+++ venv/leetcode1.py
@@ -1,27 +1,27 @@
-
 ##########reverse vowels in the string ###########################################################
 
 def reverseVovles(s="hello"):
-    v={'a','e','i','o','u','A','E','I','O','U'}
-    s=list(s)
-    i=0
-    j=len(s)-1
-    while(i<j):
-        if(s[i] not in v):
-            i+=1
+    v = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+    s = list(s)
+    i = 0
+    j = len(s) - 1
+    while (i < j):
+        if (s[i] not in v):
+            i += 1
             continue
-        if(s[j] not in v):
-            j-=1
+        if (s[j] not in v):
+            j -= 1
             continue
-        s[i],s[j]=s[j],s[i]
-        i+=1
-        j-=1
-    return(s)
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+    return (s)
+
 
 ################### palindrome by whithout using the temp variable ###################################
 
 def palindromeNum(x=121):
-    x=121
+    x = 121
     if (x < 0 or (x > 0 and x % 10 == 0)):
         return False
     n = 0
@@ -30,10 +30,9 @@ def palindromeNum(x=121):
         x = x // 10;
 
     if n == x or x == n // 10:
-       return True
+        return True
     else:
         return False
-
 
 
 # n = [ "the", "quick", "brown", "fox",
@@ -47,23 +46,24 @@ def palindromeNum(x=121):
 # w2 = "practice"
 
 ############### checking shortest distance between two words in a list #####################################
-def shortestDistance(n=["axa","ffn", "ty", "bs" ,"oin" ,"bs" ,"axa"], w1="bs", w2="axa"):
-    a=n.index(w1)
-    b=n.index(w2)
-    if a>b:
-        a,b=b,a
-    m=abs(a-b)
-    for i in range(a+1,len(n)):
-        if n[i]==n[a]:
-            a=i
-            m=min(m,abs(a-b))
-        if n[i]==n[b]:
-            b=i
-            m=min(m,abs(a-b))
+def shortestDistance(n=["axa", "ffn", "ty", "bs", "oin", "bs", "axa"], w1="bs", w2="axa"):
+    a = n.index(w1)
+    b = n.index(w2)
+    if a > b:
+        a, b = b, a
+    m = abs(a - b)
+    for i in range(a + 1, len(n)):
+        if n[i] == n[a]:
+            a = i
+            m = min(m, abs(a - b))
+        if n[i] == n[b]:
+            b = i
+            m = min(m, abs(a - b))
     return m
 
+
 #########################String isomorphic checking ###################################
-def areIsomorphic( x="pijthbsfy", y="fvladzpbf"):
+def areIsomorphic(x="pijthbsfy", y="fvladzpbf"):
     d = {}
     s = set()
     if len(x) != len(y):
@@ -82,61 +82,61 @@ def areIsomorphic( x="pijthbsfy", y="fvladzpbf"):
 
     return 1
 
+
 ######################### String Rotation ###################################
-def isRotated(str1="fsbcnuvqhffbsaqxwp",str2= "wpfsbcnuvqhffbsaqx"):
-    ac=""
-    c=""
-    ac+=str1[2:len(str1)]+str1[:2]
-    c+=str1[-2:]+str1[:len(str1)-2]
-    if((str2==c) or (str2==ac)):
+def isRotated(str1="fsbcnuvqhffbsaqxwp", str2="wpfsbcnuvqhffbsaqx"):
+    ac = ""
+    c = ""
+    ac += str1[2:len(str1)] + str1[:2]
+    c += str1[-2:] + str1[:len(str1) - 2]
+    if ((str2 == c) or (str2 == ac)):
         return 1
     else:
         return 0
 
 
+def longestSubstrDistinctChars(string="aewergrththy"):
+    seen = {}
+    maximum_length = 0
 
+    # starting the initial point of window to index 0
+    start = 0
 
-def longestSubstrDistinctChars(s="aewergrththy"):
-    k = ""
-    l = ""
-    for j in range(len(s)):
-        if s[j] in k:
-            temp=k.index(j) + 1
-            k= j
-            if len(l) < len(newString):
-                l = newString
-            newString = s[j]
-            j += 1
-        else:
-            k+=s[j]
-            j += 1
+    for end in range(len(string)):
 
-    if len(l) < len(newString):
-        l = newString
-    return len(l)
+        # Checking if we have already seen the element or not
+        if string[end] in seen:
+            # If we have seen the number, move the start pointer
+            # to position after the last occurrence
+            start = max(start, seen[string[end]] + 1)
+
+        # Updating the last seen value of the character
+        seen[string[end]] = end
+        maximum_length = max(maximum_length, end - start + 1)
+    return maximum_length
+
 
 def longestSubstrDistinctChars2(s="aewergrththy"):
     k = {}
-    k[s[0]]=0
+    k[s[0]] = 0
     newString = s[0]
     l = []
     j = 1
     while (j < len(s)):
         if s[j] in k:
-            j=k[s[j]]+1
+            j = k[s[j]] + 1
             k.clear()
             k[s[j]] = j
             l.append(newString)
             newString = s[j]
-            j+=1
+            j += 1
         else:
-            k[s[j]]=j
+            k[s[j]] = j
             newString += s[j]
-            j+=1
+            j += 1
 
     l.append(newString)
     return (len(max(l, key=len)))
-
 
 
 #############Maximum number of characters between any two same character#
@@ -171,31 +171,32 @@ def maxChars(self, s):
 
 ################Run Length Encoding#####################
 def encode(a="aasasdasfc"):
-    if len(a)==1:
-        return a+"1"
-    s=""
-    c=1
-    for i in range(1,len(a)):
-        if a[i]!=a[i-1]:
-            s+=a[i-1]+str(c)
-            c=1
+    if len(a) == 1:
+        return a + "1"
+    s = ""
+    c = 1
+    for i in range(1, len(a)):
+        if a[i] != a[i - 1]:
+            s += a[i - 1] + str(c)
+            c = 1
         else:
-            c+=1
-    s+=a[len(a)-1]+str(c)
+            c += 1
+    s += a[len(a) - 1] + str(c)
     return s
 
 
-
 from collections import Counter as c
-def isAnagram(a='b',b='d'):
+
+
+def isAnagram(a='b', b='d'):
     c1 = c(a)
     c2 = c(b)
-    if len(c1)!=len(c2):
+    if len(c1) != len(c2):
         return False
-    return c1==c2
+    return c1 == c2
 
-def ParenthesisChecker(a="((((("):
 
+def ParenthesisChecker(x="()"):
     s = ('{', '(', '[')
     stack = []
     for i in x:
@@ -214,10 +215,10 @@ def ParenthesisChecker(a="((((("):
             if i == ')':
                 if stack.pop() != '(':
                     return False
-
     if stack:
         return False
     return True
+
 
 # Input:
 # N = 4, arr[] = [1 3 2 4]
@@ -243,6 +244,7 @@ def nextLargerElement(self, arr, n):
             stack.append(arr[i])
     return result[::-1]
 
+
 # Input:
 # N = 7, price[] = [100 80 60 70 60 75 85]
 # Output:
@@ -254,12 +256,12 @@ def nextLargerElement(self, arr, n):
 # span is 1, 70 is greater than 60 so the
 # span is 2 and so on. Hence the output will
 # be 1 1 1 2 1 4 6.
-def calculateSpan(a=[100, 80, 60, 70, 60, 75 ,85]):
+def calculateSpan(a=[100, 80, 60, 70, 60, 75, 85]):
     stack = []
     stack.append(0)
     l = []
     l.append(1)
-    for i in range(1,len(a)):
+    for i in range(1, len(a)):
         while (len(stack) > 0) and (a[stack[-1]] <= a[i]):
             stack.pop()
         if not stack:
@@ -270,26 +272,26 @@ def calculateSpan(a=[100, 80, 60, 70, 60, 75 ,85]):
             stack.append(i)
     return l
 
+
 # Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal if both of them contain same set of elements, arrangements (or permutation) of elements may be different though.
 # Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
-def check(A=[1,2,5,4,0],B=[2,4,5,0,1]):
-    c1=c(A)
-    c2=c(B)
+def check(A=[1, 2, 5, 4, 0], B=[2, 4, 5, 0, 1]):
+    c1 = c(A)
+    c2 = c(B)
     for i in c1:
-        if i in c2 and c2[i]==c1[i]:
+        if i in c2 and c2[i] == c1[i]:
             continue
         else:
             return 0
     return 1
 
-def kthElement(arr1=[1,2], arr2=[3,4,5,6], k=5):
+
+def kthElement(arr1=[1, 2], arr2=[3, 4, 5, 6], k=5):
     i = 0
     j = 0
     l = []
     c = 0
     while (i < len(arr1) and j < len(arr2)):
-
-
 
         if (arr1[i] < arr2[j]):
             l.append(arr1[i])
@@ -304,6 +306,49 @@ def kthElement(arr1=[1,2], arr2=[3,4,5,6], k=5):
         l += arr1[i:]
         return l[k - 1]
     return l[k - 1]
+
+
+def wordPattern(p="abba", s="dog cat cat fish"):
+    st = set()
+    s = s.split(" ")
+    if (len(s) != len(p)):
+        return False
+    d = {}
+
+    for i in range(len(s)):
+        if s[i] in d:
+            if d[s[i]] != p[i]:
+                return False
+        else:
+            if p[i] not in s:
+                d[s[i]] = p[i]
+                s.add(p[i])
+            else:
+                return False
+    return True
+
+
+def Minimum_Index_Sum_of_Two_Lists(list1=["Shogun", "Tapioca Express", "Burger King", "KFC"],
+                                   list2=["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]):
+    d = {}
+    l = []
+    m = sys.maxsize
+    s = 0
+    for i in range(len(list1)):
+        d[list1[i]] = i
+    for j in range(len(list2)):
+        if list2[j] in d:
+            s = j + d[list2[j]]
+            if s < m:
+                m = s
+                l.clear()
+                l.append(list2[j])
+            elif s == m:
+                l.append(list2[j])
+    return l
+
+
+#print(wordPattern())
 # print(isRotated())
 # print(areIsomorphic())
 # print(shortestDistance())
@@ -316,6 +361,3 @@ def kthElement(arr1=[1,2], arr2=[3,4,5,6], k=5):
 # print(calculateSpan())
 # print(check())
 # print(kthElement())
-
-
-
